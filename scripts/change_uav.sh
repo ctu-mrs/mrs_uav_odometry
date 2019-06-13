7d669456cee3c16020014e7490aa8fac809a4133
Matej Petrlik (desktop) <petrlmat@fel.cvut.cz>
2019-06-13 16:05:16 +0200

    updated scripts

--- scripts/change_uav.sh
+++ scripts/change_uav.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# get path to script
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 if [ -x "$(whereis nvim | awk '{print $2}')" ]; then
  VIM_BIN="$(whereis nvim | awk '{print $2}')"
  HEADLESS="--headless"
@@ -8,5 +11,5 @@ elif [ -x "$(whereis vim | awk '{print $2}')" ]; then
  HEADLESS=""
 fi
 
-$VIM_BIN $HEADLESS -E -s -c "%s/uav./$1/g" -c "wqa" -- "../plot_juggler/odometry.xml"
-$VIM_BIN $HEADLESS -E -s -c "%s/uav./$1/g" -c "wqa" -- "../rviz/odometry.rviz"
+$VIM_BIN $HEADLESS -E -s -c "%s/uav./$1/g" -c "wqa" -- "$SCRIPT_PATH/../plot_juggler/odometry.xml"
+$VIM_BIN $HEADLESS -E -s -c "%s/uav./$1/g" -c "wqa" -- "$SCRIPT_PATH/../rviz/odometry.rviz"
