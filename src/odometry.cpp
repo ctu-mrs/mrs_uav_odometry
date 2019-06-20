37d53179767b9fed81425af3a0a32605e90de963
Matej Petrlik <petrlmat@fel.cvut.cz>
2019-06-20 10:13:29 +0200

    Fixed max altitude

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -5440,7 +5440,7 @@ namespace mrs_odometry
 
     mrs_lib::Routine profiler_routine = profiler->createRoutine("callbackMavrosDiag");
 
-    max_altitude = _max_default_altitude;
+    /* max_altitude = _max_default_altitude; */
 
     {
       std::scoped_lock lock(mutex_mavros_diag);
