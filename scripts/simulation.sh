3c6f7c2a58efd417fa0c7d6b41cfd2f42ff7434a
Matej Petrlik <petrlmat@fel.cvut.cz>
2020-02-11 17:28:56 +0100

    simulation script switches worlds

--- scripts/simulation.sh
+++ scripts/simulation.sh
@@ -5,6 +5,7 @@ UAV_NAME=$SESSION_NAME
 ESTIMATOR=$1
 SENSORS=
 LAUNCH_NODES=
+WORLD='$(find simulation)/worlds/grass_plane.world'
 
 if [ "$#" == 0 ]; then
   ESTIMATOR=gps
@@ -13,15 +14,19 @@ fi
 if [ "$ESTIMATOR" == "optflow" ]; then
   SENSORS="$SENSORS --enable-bluefox-camera"
   LAUNCH_NODES='waitForOdometry; roslaunch mrs_optic_flow optic_flow.launch'
+  WORLD='worlds/grass_plane.world'
 elif [ "$ESTIMATOR" == "hector" ]; then
   SENSORS="$SENSORS --enable-rplidar"
   LAUNCH_NODES='waitForOdometry; roslaunch hector_mapping uav.launch'
+  WORLD='worlds/cathedral.world'
 elif [ "$ESTIMATOR" == "icp" ]; then
   SENSORS="$SENSORS --enable-rplidar"
   LAUNCH_NODES='waitForOdometry; roslaunch mrs_icp2d uav.launch'
+  WORLD='worlds/cathedral.world'
 elif [ "$ESTIMATOR" == "lidar" ]; then
   SENSORS="$SENSORS --enable-velodyne"
   LAUNCH_NODES='waitForOdometry; roslaunch aloam_velodyne velodyne_odometry_simulation.launch'
+  WORLD='worlds/cathedral.world'
 fi
 
 # following commands will be executed first, in each window
@@ -32,7 +37,7 @@ pre_input="export UAV_NAME=$UAV_NAME; export ATHAME_ENABLED=0; export ROS_MASTER
 input=(
   'Roscore' 'roscore
 '
-  'Gazebo' "waitForRos; roslaunch simulation simulation.launch gui:=true debug:=false
+  'Gazebo' "waitForRos; roscd simulation; roslaunch simulation simulation.launch gui:=true debug:=false world_name:=$WORLD
 "
   'Spawn' "waitForSimulation; spawn 1 --run --delete --$UAV_TYPE --enable-rangefinder --enable-ground-truth $SENSORS --file ~/mrs_workspace/src/uav_core/ros_packages/mrs_odometry/config/init_pose/init_pose.csv
 "
