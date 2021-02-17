926b81d989c279f634e7df08a1c0cee7e0c07d2c
uav10 <uav10>
2021-02-17 15:19:49 +0100

    increased toleration for aloamgarm median filter when close to the ground

--- src/AltitudeEstimatorAloamGarm.cpp
+++ src/AltitudeEstimatorAloamGarm.cpp
@@ -344,7 +344,7 @@ bool AltitudeEstimatorAloamGarm::doCorrection(const double &measurement, int mea
 
     // TODO some better solution? (bigger difference from median allowed when close to the ground)
     if (measurement_name == "height_range" && !m_median_filter->isValid(z(0)) && m_median_filter->isFilled() &&
-        (z(0) > 0.4 || fabs(m_median_filter->getMedian() - z(0)) > 0.2)) {
+        (z(0) > 0.6 || fabs(m_median_filter->getMedian() - z(0)) > 0.5)) {
       // set H matrix so that only garmin bias is updated
       /* lkf_id = m_garmin_bias_only_id; */
       // calculate new bias measurement
